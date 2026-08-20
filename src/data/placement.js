a999aba0426ceda531ca7546fabf1b0be1bc28bb
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-08-20 19:23:44 +0530

    Add files via upload

---
+++ src/data/placement.js
@@ -0,0 +1,54 @@
+import google from "../assets/images/google.png";
+import microsoft from "../assets/images/microsoft.png";
+import amazon from "../assets/images/amazon.png";
+import oracle from "../assets/images/oracle.jpg";
+import flipkart from "../assets/images/flipkart.png";
+import infosys from "../assets/images/infosys.webp";
+
+
+const placements = [
+  {
+    name: "Rahul Roy",
+    image: "https://randomuser.me/api/portraits/men/32.jpg",
+    company: "Google",
+    icon: google,
+    lpa: "32 LPA",
+  },
+  {
+    name: "Priya Verma",
+    image: "https://randomuser.me/api/portraits/women/44.jpg",
+    company: "Microsoft",
+    icon: microsoft,
+    lpa: "28 LPA",
+  },
+  {
+    name: "Amit Kumar",
+    image: "https://randomuser.me/api/portraits/men/67.jpg",
+    company: "Amazon",
+    icon: amazon,
+    lpa: "25 LPA",
+  },
+  {
+    name: "Sneha Patel",
+    image: "https://randomuser.me/api/portraits/women/65.jpg",
+    company: "Oracle",
+    icon: oracle,
+    lpa: "22 LPA",
+  },
+  {
+    name: "Vikram Singh",
+    image: "https://randomuser.me/api/portraits/men/81.jpg",
+    company: "Flipkart",
+    icon: flipkart,
+    lpa: "18 LPA",
+  },
+  {
+    name: "Ananya Roy",
+    image: "https://randomuser.me/api/portraits/women/21.jpg",
+    company: "Infosys",
+    icon: infosys,
+    lpa: "12 LPA",
+  },
+];
+
+export default placements;
