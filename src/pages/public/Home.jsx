4fc0f68c2d4e05087df02a5261a872e2b835eecb
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-08-22 09:42:25 +0530

    Add files via upload

---
+++ src/pages/public/Home.jsx
@@ -0,0 +1,19 @@
+import Features from "../../components/home/Features";
+import Hero from "../../components/home/Hero";
+import Stats from "../../components/home/Stats";
+import TrustedCompanies from "../../components/home/TrustedCompanies";
+import WhyChooseUs from "../../components/home/WhyChooseUs";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <TrustedCompanies />
+      <Features />
+      <WhyChooseUs />
+      <Stats />
+    </>
+  );
+};
+
+export default Home;
